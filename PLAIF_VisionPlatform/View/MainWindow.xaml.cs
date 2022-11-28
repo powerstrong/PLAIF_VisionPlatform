337351b9a9c5348a8f4ed0408a7dc002eacbe3e6
jongwooparkk <jwpark@plaif.com>
2022-11-28 17:16:53 +0900

    1. Image Zoom In/Out 기능 추가 2. nullable ? 를 추가하여 경고메세지 줄임

--- PLAIF_VisionPlatform/View/MainWindow.xaml.cs
+++ PLAIF_VisionPlatform/View/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using PLAIF_VisionPlatform.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,8 +13,11 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using Image = System.Windows.Controls.Image;
 
 namespace PLAIF_VisionPlatform
 {
@@ -21,9 +26,58 @@ namespace PLAIF_VisionPlatform
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        /// <summary>
+        /// 원본 포인트입니다.
+        /// </summary>
+        private Point origin_2D;
+
+        /// <summary>
+        /// 시작 포인트입니다.
+        /// </summary>
+        private Point start_2D;
+
+        /// <summary>
+        /// 원본 포인트입니다.
+        /// </summary>
+        private Point origin_3D;
+
+        /// <summary>
+        /// 시작 포인트입니다.
+        /// </summary>
+        private Point start_3D;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            #region 2D Image Zoom In/Out에 대한 정의
+            TransformGroup transformGroup_2D = new TransformGroup();
+            ScaleTransform scaleTransform_2D = new ScaleTransform();
+            transformGroup_2D.Children.Add(scaleTransform_2D);
+            TranslateTransform translateTransform_2D = new TranslateTransform();
+            transformGroup_2D.Children.Add(translateTransform_2D);
+            Image_2D_Control.RenderTransform = transformGroup_2D;
+
+            Image_2D_Control.MouseWheel += Image_MouseWheel;
+            Image_2D_Control.MouseLeftButtonDown += Image_MouseDown;
+            Image_2D_Control.MouseLeftButtonUp += Image_MouseUp;
+            Image_2D_Control.MouseMove += Image_MouseMove;
+            #endregion
+
+            #region Depth Image Zoom In/Out에 대한 정의
+            TransformGroup transformGroup_3D = new TransformGroup();
+            ScaleTransform scaleTransform_3D = new ScaleTransform();
+            transformGroup_3D.Children.Add(scaleTransform_3D);
+            TranslateTransform translateTransform_3D = new TranslateTransform();
+            transformGroup_3D.Children.Add(translateTransform_3D);
+            Image_Depth_Control.RenderTransform = transformGroup_3D;
+
+            Image_Depth_Control.MouseWheel += Image_MouseWheel;
+            Image_Depth_Control.MouseLeftButtonDown += Image_MouseDown;
+            Image_Depth_Control.MouseLeftButtonUp += Image_MouseUp;
+            Image_Depth_Control.MouseMove += Image_MouseMove;
+            #endregion
 
             DataContext = new MainViewModel();
         }
@@ -36,31 +90,113 @@ namespace PLAIF_VisionPlatform
 
         private void Image_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            //확대 되는 마우스 위치 포인트를 체크 할 것
-            //축소는 제한 비율은??
-            if(e.Delta > 0)
-            {
-                myScaleTransform.ScaleX *= 1.1;
-                myScaleTransform.ScaleY *= 1.1;
+            Image? img = sender as Image;
 
-                if (myScaleTransform.ScaleX > 2)
+            if (img != null)
+            {
+                var transform = (ScaleTransform)((TransformGroup)img.RenderTransform).Children.First(c => c is ScaleTransform);
+                double zoom = e.Delta > 0 ? .1 : -.1;
+
+                transform.ScaleX += zoom;
+                transform.ScaleY += zoom;
+
+                if (transform.ScaleX > 2) transform.ScaleX = 2;
+                if (transform.ScaleY > 2) transform.ScaleY = 2;
+                if (transform.ScaleX < 1) transform.ScaleX = 1;
+                if (transform.ScaleY < 1) transform.ScaleY = 1;
+            }
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Image? img = sender as Image;
+
+            Point start;
+            Point origin;
+            Border border;
+
+            if (img != null)
+            {
+                switch (img.Name)
                 {
-                    myScaleTransform.ScaleX = 2;
-                    myScaleTransform.ScaleY = 2;
+                    default:
+                    case "Image_2D_Control":
+                        border = border2D;
+                        break;
+                    case "Image_Depth_Control":
+                        border = border3D;
+                        break;
+                }
+
+                img.CaptureMouse();
+                var translateTransform = (TranslateTransform)((TransformGroup)img.RenderTransform).Children.First(c => c is TranslateTransform);
+                start = e.GetPosition(border);
+                origin = new Point(translateTransform.X, translateTransform.Y);
+
+                switch (img.Name)
+                {
+                    default:
+                    case "Image_2D_Control":
+                        start_2D = start;
+                        origin_2D = origin;
+                        break;
+                    case "Image_Depth_Control":
+                        start_3D = start;
+                        origin_3D = origin;
+                        break;
                 }
             }
-            else
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Image? img = sender as Image;
+            if (img != null)
             {
-                myScaleTransform.ScaleX /= 1.1;
-                myScaleTransform.ScaleY /= 1.1;
+                img.ReleaseMouseCapture();
+            }            
+        }
 
-                if (myScaleTransform.ScaleX < 1)
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            Image? img = sender as Image;
+            Point start;
+            Point origin;
+            Border border;
+
+            if (img != null)
+            {
+                if (!img.IsMouseCaptured) return;
+
+                switch (img.Name)
                 {
-                    myScaleTransform.ScaleX = 1;
-                    myScaleTransform.ScaleY = 1;
+                    default:
+                    case "Image_2D_Control":
+                        start = start_2D;
+                        origin = origin_2D;
+                        border = border2D;
+                        break;
+                    case "Image_Depth_Control":
+                        start = start_3D;
+                        origin = origin_3D;
+                        border = border3D;
+                        break;
                 }
-            }
 
+                var translateTransform = (TranslateTransform)((TransformGroup)img.RenderTransform).Children.First(c => c is TranslateTransform);
+                var scaleTransform = (ScaleTransform)((TransformGroup)img.RenderTransform).Children.First(c => c is ScaleTransform);
+                Vector v = start - e.GetPosition(border);
+                translateTransform.X = origin.X - v.X;
+                translateTransform.Y = origin.Y - v.Y;
+
+                double LimitPoseX = ((img.Width * scaleTransform.ScaleX) - img.Width) / 2;
+                double LimitPoseY = ((img.Height * scaleTransform.ScaleY) - img.Height) / 2;
+
+                if (translateTransform.X > LimitPoseX) translateTransform.X = LimitPoseX;
+                if (translateTransform.Y > LimitPoseY) translateTransform.Y = LimitPoseY;
+                if (translateTransform.X < -LimitPoseX) translateTransform.X = -LimitPoseX;
+                if (translateTransform.Y < -LimitPoseY) translateTransform.Y = -LimitPoseY;
+            }  
         }
     }
 }
--- PLAIF_VisionPlatform/ViewModel/Command/DelegateCommand.cs
+++ PLAIF_VisionPlatform/ViewModel/Command/DelegateCommand.cs
@@ -9,10 +9,10 @@ namespace PLAIF_VisionPlatform.ViewModel.Command
 {
     public class DelegateCommand : ICommand
     {
-        readonly Action<object> _execute;
-        readonly Predicate<object> _canExecute;
+        readonly Action<object?> _execute;
+        readonly Predicate<object?> _canExecute;
 
-        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        public DelegateCommand(Action<object?> execute, Predicate<object?> canExecute)
         {
             if (execute == null)
                 throw new NullReferenceException("execute can not null");
@@ -21,23 +21,23 @@ namespace PLAIF_VisionPlatform.ViewModel.Command
             _canExecute = canExecute;
         }
 
-        public DelegateCommand(Action<object> execute) : this(execute, null)
+        public DelegateCommand(Action<object?> execute) : this(execute, null)
         {
 
         }
 
-        public event EventHandler CanExecuteChanged
+        public event EventHandler? CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public bool CanExecute(object parameter)
+        public bool CanExecute(object? parameter)
         {
             return _canExecute == null ? true : _canExecute(parameter);
         }
 
-        public void Execute(object parameter)
+        public void Execute(object? parameter)
         {
             _execute.Invoke(parameter);
         }
