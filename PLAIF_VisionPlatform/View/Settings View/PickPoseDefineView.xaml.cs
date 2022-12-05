672cb8b7ba2bc0c5099b9c988d4a1d64071e49b1
jongwooparkk <jwpark@plaif.com>
2022-12-05 11:49:03 +0900

    1. Pick Pose Define 창 센터로 띄우기 2. Connection 관련 임시 코드 원복

--- PLAIF_VisionPlatform/View/Settings View/PickPoseDefineView.xaml.cs
+++ PLAIF_VisionPlatform/View/Settings View/PickPoseDefineView.xaml.cs
@@ -22,6 +22,13 @@ namespace PLAIF_VisionPlatform.View.Settings_View
         public PickPoseDefineView()
         {
             InitializeComponent();
+
+            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            double windowWidth = this.Width;
+            double windowHeight = this.Height;
+            this.Left = (screenWidth / 2) - (windowWidth / 2);
+            this.Top = (screenHeight / 2) - (windowHeight / 2);
         }
     }
 }
